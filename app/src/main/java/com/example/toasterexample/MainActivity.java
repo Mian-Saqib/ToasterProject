3255f16a7a927ef51dc463b2b2e4c14ee445456a
Mian Saqib <saqibrza7@gmail.com>
2021-12-10 09:42:25 +0500

    first commit

---
+++ app/src/main/java/com/example/toasterexample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.toasterexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.toasterlibrary.ToasterMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ToasterMessage.s(MainActivity.this,"Hello World");
+
+    }
+}
